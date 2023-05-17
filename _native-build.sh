25d39b5f2905ed2d7b60a58e865615910362c310
Peter Veres <peter@surepay.nl>
2023-05-17 10:55:44 +0200

    Added variables and comments to build scripts.

--- _native-build.sh
+++ _native-build.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env sh
 
+NATIVE_BUILD_DIR=$(pwd)/target/native
+INPUT_JAR_WITH_NATIVE_HINTS=../native-test.jar
+OUTPUT_EXECUTABLE_NAME=native-test-trained
+
+
 echo "Run GraalVM native build using previously generated hints."
-rm -rf target/native
-mkdir -p target/native
-cd target/native
-jar -xvf ../native-test.jar
-$GRAALVM_HOME/bin/native-image -H:Name=native-test-trained -cp .:BOOT-INF/classes:`find BOOT-INF/lib | tr '\n' ':'`
-mv native-test-trained ../
+echo "Prepare native build dir."
+rm -rf "$NATIVE_BUILD_DIR"
+mkdir -p "$NATIVE_BUILD_DIR"
+cd "$NATIVE_BUILD_DIR" || exit 1
+
+echo "Unpack jar."
+jar -xvf $INPUT_JAR_WITH_NATIVE_HINTS
+
+echo "Run native build."
+"$GRAALVM_HOME"/bin/native-image -H:Name=$OUTPUT_EXECUTABLE_NAME -cp .:BOOT-INF/classes:`find BOOT-INF/lib | tr '\n' ':'`
+mv $OUTPUT_EXECUTABLE_NAME ../
--- _train-and-trace.sh
+++ _train-and-trace.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
-# Runtime (in local without config/optimization) ~4 min
+# Runtime depends only on the training.
+
+INPUT_JAR_PATH="$(pwd)"/target/native-test.jar
 
 echo "Run the native image tracing agent to pre-train the native build. \
 The result of consecutive runs will be merged. \
 Just stop the running process once training is complete!"
-$GRAALVM_HOME/bin/java \
+"$GRAALVM_HOME"/bin/java \
     -Dspring.aot.enabled=true \
     -agentlib:native-image-agent=config-merge-dir="$(pwd)"/target/classes/META-INF/native-image \
-    -jar "$(pwd)"/target/native-test.jar
+    -jar "$INPUT_JAR_PATH"
 
-echo "Creating a safety copy of the generated hints to be able to skip this step in the future"
+echo "Create a safety copy of the generated hints to be able to skip this step in the future"
 mkdir -p $(pwd)/native-hints-backup/post-train
 cp -rf $(pwd)/target/classes/META-INF $(pwd)/native-hints-backup/post-train/
 
